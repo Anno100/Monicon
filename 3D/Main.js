31a211e2b742c63def53be56363783a0035712df
Anno100 <alessmon.acoro@googlemail.com>
2024-08-02 20:15:59 +0200

    Add better UI

--- 3D/Main.js
+++ 3D/Main.js
@@ -28,10 +28,52 @@ function is_touch_enabled() {
 
 function Main() {
     console.clear();
-    createFormButton('Home');
-    createFormButton('','Description');
+    let toDescription = $create('a');
+    toDescription.href = 'https://anno100.github.io/Monmon-Gaming/'
+    toDescription.innerHTML = 'See Description'
+    document.body.appendChild(toDescription);
+    
+    
+
+
 
     let canvas_3d = Canvas3D.createSBSCanvas();
+
+    let run_button = $create('button');
+    run_button.innerHTML = 'Run'
+    run_button.style.width = '100px';
+    run_button.style.height = '100px';
+    run_button.style.marginLeft = window.innerWidth/2-50 + 'px';
+    run_button.style.marginTop =  window.innerHeight/2-50 + 'px';
+    run_button.onclick = () => {
+
+        let run_anim = setInterval(()=>{
+            if(Number(run_button.style.height.split('px')[0]) < window.innerHeight*2){
+                run_button.style.width = (Number(run_button.style.width.split('px')[0])+100) + 'px';
+                run_button.style.marginLeft = (Number(run_button.style.marginLeft.split('px')[0]) - 50) + 'px';
+                run_button.style.height = (Number(run_button.style.height.split('px')[0])+100) + 'px';
+                run_button.style.marginTop = (Number(run_button.style.marginTop.split('px')[0]) - 50) + 'px';
+            }
+            else{
+
+                canvas_3d.view.requestFullscreen();
+        
+                setTimeout(() => {
+                canvas_3d.left.setWidth(window.innerWidth / 2);
+                canvas_3d.right.setWidth(window.innerWidth / 2);
+                canvas_3d.left.setHeight(window.innerHeight);
+                canvas_3d.right.setHeight(window.innerHeight);
+                
+            }, 100);
+            if(Number(run_button.style.height.split('px')[0]) >= window.innerHeight*2)
+            run_button.style.visibility = 'hidden';
+        clearInterval(run_anim);
+            }
+
+        },100);
+        
+    };
+    document.body.appendChild(run_button);
 
     let M = { x: -100, y: -100 }
     let onLeftSide = true;
